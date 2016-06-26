2b0194da895f912d47d8a7147f66bbc79d67b612
danielmoi <daniel@ampersandmoi.com>
2016-06-26 11:06:58 +1000

    Finish our own `createStore` method

--- counter.js
+++ counter.js
@@ -10,25 +10,38 @@ const counter = (state = 0, action) => {
 }
 
 // get `createStore` method from Redux
-const { createStore } = Redux;
+// const { createStore } = Redux;
 
 // our own `createStore` method
 const createStore = (reducer) => {
   // keep our state in a variable
   let state;
 
+  // keep track of all the change listeners,
+  // because the `subscribe` function can be called many times
+  let listeners = [];
+
   // our `getState` method: returns our state (implicit return)
   const getState = () => state;
 
   // our `dispatch` method
   const dispatch = (action) => {
-
+    state = reducer(state, action);
+    listeners.forEach( listener => listener() );
   };
 
   // our `subscribe` method
   const subscribe = (listener) => {
+    listeners.push(listener);
 
+    // Instead of adding a dedicated `unsubscribe` method, we'll just return a function from subscribe method.
+    return () => {
+      listeners = listeners.filter( l => l !== listener);
+    };
   };
+
+  // Finally, by the time the store is returned, we want it to have the initial state populated, so we're going to dispatch a dummy action, just to get the reducer to return the initial value
+  dispatch( {} );
 
   // returns an object with 3 functions
   // this object is called our Redux STORE
