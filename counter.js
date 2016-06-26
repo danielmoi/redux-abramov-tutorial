622be3c1317dabfe4c27f209784dfc0974376e7f
danielmoi <daniel@ampersandmoi.com>
2016-06-26 10:46:27 +1000

    Start our own `createStore` method

--- counter.js
+++ counter.js
@@ -1,5 +1,3 @@
-const { createStore } = Redux;
-
 const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
@@ -11,7 +9,36 @@ const counter = (state = 0, action) => {
   }
 }
 
-// create store
+// get `createStore` method from Redux
+const { createStore } = Redux;
+
+// our own `createStore` method
+const createStore = (reducer) => {
+  // keep our state in a variable
+  let state;
+
+  // our `getState` method: returns our state (implicit return)
+  const getState = () => state;
+
+  // our `dispatch` method
+  const dispatch = (action) => {
+
+  };
+
+  // our `subscribe` method
+  const subscribe = (listener) => {
+
+  };
+
+  // returns an object with 3 functions
+  // this object is called our Redux STORE
+  return { getState, dispatch, subscribe };
+};
+
+
+
+
+// create store using `createStore` method
 const myStore = createStore(counter);
 
 const render = () => {
