988fc5979748626202a9d36248e6dd3848667945
danielmoi <daniel@ampersandmoi.com>
2016-08-28 13:50:03 +1000

    Finish video #10 Colocating selectors with reducers

--- js/reducers/todos.js
+++ js/reducers/todos.js
@@ -41,3 +41,17 @@ const todos = (state=[], action) => {
 };
 
 export default todos;
+
+// our selector function, colocated with reducers
+export const getVisibleTodos = (state, filter) => {
+  switch (filter) {
+    case 'all':
+      return state;
+    case 'completed':
+      return state.filter(t => t.completed);
+    case 'active':
+      return state.filter(t => !t.completed);
+    default:
+      throw new Error(`Unknown filter: ${filter}.`);
+  }
+};
