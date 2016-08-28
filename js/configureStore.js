45c2e7a6a90441e9fac1a1450ee35a932ab5c3d2
danielmoi <daniel@ampersandmoi.com>
2016-08-28 15:39:58 +1000

    Finish video #13 Implement fake backend using Promise

--- js/configureStore.js
+++ js/configureStore.js
@@ -1,14 +1,15 @@
 import { createStore } from 'redux';
 import todoApp from './reducers';
-import { loadState, saveState } from './localStorage';
-import throttle from 'lodash/throttle';
+// import { loadState, saveState } from './localStorage';
+// import throttle from 'lodash/throttle';
 
 
 const configureStore = () => {
 
   // Let's give our app some persisted initial state using local storage
   // (instead of declaring an object literal)
-  const persistedState = loadState();
+  // (now removed, in video #13)
+  // const persistedState = loadState();
 
   const addLoggingToDispatch = (store) => {
     if(!console.group) {
@@ -33,11 +34,10 @@ const configureStore = () => {
   };
 
 
-  // Redux allows us to pass the persisted state as the 2nd argument
+  // Redux allows us to pass the persisted state as the 2nd argument (now removed, in video #13)
   // The 1st argument is the root reducer
   const store = createStore(
-    todoApp,
-    persistedState
+    todoApp
   );
 
   // this is an environment check
@@ -48,15 +48,15 @@ const configureStore = () => {
 
   // This adds a listener that will be called every time the store changes
   // (invoked on any state change)
-  store.subscribe(throttle(() => {
-    // get Redux store state using Redux getState()
-    // We will only persis the data (todo list)
-    // and NOT the UI state (visibility Filter)
-    console.log('throttling...');
-    saveState({
-      todos: store.getState().todos,
-    });
-  }), 10000);
+  // store.subscribe(throttle(() => {
+  //   // get Redux store state using Redux getState()
+  //   // We will only persis the data (todo list)
+  //   // and NOT the UI state (visibility Filter)
+  //   console.log('throttling...');
+  //   saveState({
+  //     todos: store.getState().todos,
+  //   });
+  // }), 10000);
 
   return store;
 };
--- js/index.js
+++ js/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './configureStore';
 import Root from './components/Root';
+import { fetchTodos } from './api';
 
 // Move configuration into configureStore.js
+
+fetchTodos('all')
+  .then(todos =>
+    console.log('todos:', todos)
+)
 
 // We have to call `configureStore` to return the `store` object
 const store = configureStore();
